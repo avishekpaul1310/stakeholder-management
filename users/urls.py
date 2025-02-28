57ad21976a8f770c64838cb64a22e9d5bde36193
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-28 10:21:04 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,3 @@
-# users/urls.py - Update the logout URL pattern
-
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -7,6 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Updated logout path - Use template_name instead of next_page
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
