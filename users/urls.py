a4020c9c5f43b979828661de72313e40d12c375a
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-28 09:26:36 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
-# users/urls.py
+# users/urls.py - Replace with this content
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -6,6 +7,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
